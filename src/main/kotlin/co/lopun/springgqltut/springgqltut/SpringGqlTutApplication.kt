6463bdb0d14d319467aaff885499b24b79d814a8
고지훈 <ujin43255252@gmail.com>
2019-09-07 14:15:41 +0900

    :tada: Initialize Project

---
+++ src/main/kotlin/co/lopun/springgqltut/springgqltut/SpringGqlTutApplication.kt
@@ -0,0 +1,11 @@
+package co.lopun.springgqltut.springgqltut
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringGqlTutApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringGqlTutApplication>(*args)
+}
